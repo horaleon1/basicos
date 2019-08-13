f7e37d0c3d7fe68a63ef27d4f0523c736a9f25f8
horaleon1 <horaleon1@gmail.com>
2019-08-12 20:08:57 -0500

    probando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+ 
 
 function App() {
+  const empleado = {
+    nombre: 'Juan Pablo',
+    trabajo: 'Desarrollador Web'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment >
+      <h1> { empleado.nombre } </h1>
+      <p> { empleado.trabajo } </p>
+    </Fragment>
   );
 }
 
